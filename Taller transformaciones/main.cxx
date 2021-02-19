6de6a35c3f3b03be0bbc62bc8a031eca57453c87
Alejandro <57674935+srmcfly@users.noreply.github.com>
2021-02-19 16:10:21 -0500

    1.0 (#1)
    * 1.0
    
    * taller trans 2
    
    * taller trans 3

--- Taller transformaciones/main.cxx
+++ Taller transformaciones/main.cxx
@@ -4,44 +4,60 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
-int main( int argc, char** argv )
+
+void metodoFunciones ( Mat img1){
+
+    Mat img2, img3, final;
+    resize(img1, img2, Size(), 0.8, 0.8);
+    Point2f punto(img2.cols/2., img2.rows/2.); 
+    Mat M = getRotationMatrix2D(punto, -35, 1);
+    warpAffine(img2, img3, M, Size(img2.cols, img2.rows));
+    Mat trans_mat = (Mat_<double>(2,3) << 1, 0, -42, 0, 1, 37);
+    warpAffine( img3, final, trans_mat, img1.size());
+    imwrite("final_metodo_funciones.jpg", final);
+    return;
+}
+
+void metodo2Matriz ( Mat img1){
+
+    Mat img2, img3, final;
+
+    Point2f srcTri[3];
+    srcTri[0] = Point2f( 0.f, 0.f );
+    srcTri[1] = Point2f( img1.cols - 1.f, 0.f );
+    srcTri[2] = Point2f( 0.f, img1.rows - 1.f );
+    Point2f dstTri[3];
+    dstTri[0] = Point2f( 0.f, img1.rows*0.33f );
+    dstTri[1] = Point2f( img1.cols*0.85f, img1.rows*0.25f );
+    dstTri[2] = Point2f( img1.cols*0.15f, img1.rows*0.7f );
+
+    Mat warp_dst = Mat::zeros( img1.rows, img1.cols, img1.type() );
+
+    Point centro = Point( (warp_dst.cols/2) , (warp_dst.rows/2));
+    double angulo = -35;
+    double rescalado = 0.8;
+    Mat matrizRotacion = getRotationMatrix2D( centro, angulo, rescalado );
+
+    warpAffine( img1, img2, matrizRotacion, img1.size() );
+    Mat trans_mat = (Mat_<double>(2,3) << 1, 0, -42, 0, 1, 37);
+    warpAffine( img2, img3, trans_mat, img1.size());
+
+    imwrite( "final_metodo_matriz.jpg", img3 );
+    return;
+}
+int main(int argc, char **argv)
 {
-    Mat src = imread( "ricardo.jpg",1  );
-    if( src.empty() )
+    Mat img1 = imread(argv[1], 1), img2, img3, final;
+    if (img1.empty())
     {
-        cout << "Could not open or find the image!\n" << endl;
+        cout << "Could not open or find the image!\n"
+             << endl;
         cout << "Usage: " << argv[0] << " <Input image>" << endl;
         return -1;
     }
-    
-    Point2f srcTri[3];
-    srcTri[0] = Point2f( 0.f, 0.f );
-    srcTri[1] = Point2f( src.cols - 1.f, 0.f );
-    srcTri[2] = Point2f( 0.f, src.rows - 1.f );
-    Point2f dstTri[3];
-    dstTri[0] = Point2f( 0.f, src.rows*0.33f );
-    dstTri[1] = Point2f( src.cols*0.85f, src.rows*0.25f );
-    dstTri[2] = Point2f( src.cols*0.15f, src.rows*0.7f );
 
-    Mat warp_dst = Mat::zeros( src.rows, src.cols, src.type() );
-    
-    //warpAffine( src, warp_dst, warp_mat, warp_dst.size() );
-    
-    Point center = Point( (warp_dst.cols/2) , (warp_dst.rows/2));
-    double angle = -35;
-    double scale = 0.8;
-    Mat rot_mat = getRotationMatrix2D( center, angle, scale );
-    Mat warp_rotate_dst, trans;
-    
-    warpAffine( src, warp_rotate_dst, rot_mat, src.size() );
-    
-
-    Mat trans_mat = (Mat_<double>(2,3) << 1, 0, -42, 0, 1, 37);
-    warpAffine( warp_rotate_dst, trans, trans_mat, src.size());
-    
-    imwrite( "Source image.jpg", src );
-    imwrite( "Warp + Rotate.jpg", warp_rotate_dst );
-    imwrite( "Warp + affafafaf.jpg", trans );
-    
+    metodoFunciones(img1);
+    metodo2Matriz(img1);
+ 
     return 0;
 }
